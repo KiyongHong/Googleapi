f01502b65e466b1fea750ce1418276536b898591
HKY <starmonhong@gmail.com>
2016-05-24 05:00:33 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Googleapi::Application.config.secret_key_base = 'aad311ce8b81c7784aee711a962151f9d0c8d0468711b4738b2eaff66b6c45946f1dd03fe400337742b0affe848b65168d89f523a064d620ab033358171a5fb7'
